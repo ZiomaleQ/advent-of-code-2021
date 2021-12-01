0b7f6526f8df292282a88335f326f567165d567d
ZiomaleQ <r.partyka30@gmail.com>
2021-12-01 15:04:29 +0100

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var lastNum = input[0].toInt()
+
+        for (line in input) {
+            val currNum = line.toInt()
+            lastNum = currNum.also { if (lastNum < currNum) count++ }
+        }
+
+        return count
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var end = false
+
+        fun getTriple(offset: Int): Triple<Int, Int, Int> = if (input.size - offset >= 3) {
+            Triple(input[offset].toInt(), input[offset + 1].toInt(), input[offset + 2].toInt())
+        } else Triple(0, 0, 0).also { end = true }
+
+        var offset = 0
+        var triplet = getTriple(offset)
+
+        while (!end) {
+            val currTriplet = getTriple(++offset)
+            triplet = currTriplet.also { if (triplet.sum() < currTriplet.sum()) count++ }
+        }
+
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val baseInput = readInput("Day01")
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Answer for first part: ${part1(baseInput)}")
+    println("Answer for second part: ${part2(baseInput)}")
 }
+
+fun Triple<Int, Int, Int>.sum(): Int = this.first + this.second + this.third
